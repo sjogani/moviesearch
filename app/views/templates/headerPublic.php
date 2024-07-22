00d3d430ee23cb0a6faa4d26746e79e2538073c3
sjogani <123866376+sjogani@users.noreply.github.com>
2024-07-22 01:29:06 +0000

    Added header to the starting window

--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -15,3 +15,33 @@ if (isset($_SESSION['auth']) == 1) {
     <meta name="mobile-web-app-capable" content="yes">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">COSC 4806</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="/home">Home</a>
+                </li>
+                
+                  <?php
+                  if(!isset($_SESSION['authenticated'])) {
+                      echo "<li class='nav-item'>
+                            <a href='/home' class='nav-link'>Login</a>
+                            </li>";
+                  } else {
+
+                    echo "<li class='nav-item'>
+                            <a href='/logout.php' class='nav-link'>Logout</a>
+                            </li>";
+                  }
+                  ?>
+
+              </ul>
+            </div>
+          </div>
+        </nav>
+    </body>
